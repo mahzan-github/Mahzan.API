2b2196b0f02f355ee569a4a629fc16692c4ceb4e
Carlos Maldonado <carlos.maldonado@conekta.com>
2021-03-12 15:01:13 -0600

    Creacion de Product Sale Units Repository

--- src/Mahzan.API/Application/Requests/ProductSaleUnits/CreateProductSaleUnitRequest.cs
+++ src/Mahzan.API/Application/Requests/ProductSaleUnits/CreateProductSaleUnitRequest.cs
@@ -1,7 +1,13 @@
+using System;
+
 namespace Mahzan.API.Application.Requests.ProductSaleUnits
 {
     public class CreateProductSaleUnitRequest
     {
+        public string  Abbreviation { get; set; }
         
+        public string  Description { get; set; }
+        
+        public Guid  CompanyId { get; set; }  
     }
 }
--- src/Mahzan.API/Controllers/V1/ProductSaleUnitsController.cs
+++ src/Mahzan.API/Controllers/V1/ProductSaleUnitsController.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Net;
 using System.Threading.Tasks;
 using Mahzan.API.Application.Requests.ProductSaleUnits;
+using Mahzan.Persistance.V1.Dto.ProductSaleUnits;
+using Mahzan.Persistance.V1.Repositories.ProductSaleUnits.CreateProductSaleUnit;
 using Mahzan.Persistance.V1.ViewModel.ProductSaleUnits;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,12 +14,40 @@ namespace Mahzan.API.Controllers.V1
     [ApiController]
     public class ProductSaleUnitsController : Controller
     {
+        private readonly ICreateProductSaleUnitRepository _createProductSaleUnitRepository;
+
+        public ProductSaleUnitsController(
+            ICreateProductSaleUnitRepository createProductSaleUnitRepository)
+        {
+            _createProductSaleUnitRepository = createProductSaleUnitRepository;
+        }
+
+
         [HttpPost("product-sale-units:create")]
         [ProducesResponseType(typeof(CreateProductSaleUnitViewModel), (int) HttpStatusCode.OK)]
-        public async Task<IActionResult> CreateProductPurchaseUnit(CreateProductSaleUnitRequest request)
+        public async Task<IActionResult> CreateProductPurchaseUnit(
+            CreateProductSaleUnitRequest request)
         {
-
-            return Ok();
+            CreateProductSaleUnitDto createProductSaleUnitDto;
+            try
+            {
+                createProductSaleUnitDto =await _createProductSaleUnitRepository
+                    .Insert(new CreateProductSaleUnitDto
+                    {
+                        Abbreviation = request.Abbreviation,
+                        Description = request.Description,
+                        CompanyId = request.CompanyId
+                    });
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            return Ok(new CreateProductSaleUnitViewModel
+            {
+                ProductSaleUnitId = createProductSaleUnitDto.ProductSaleUnitId
+            });
         }
     }
 }
